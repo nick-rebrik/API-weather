16ae9136e1aca1bb5b06463924e20515a24451c7
wwl-nick <131856519+wwl-nick@users.noreply.github.com>
2023-10-12 09:07:00 +0200

    added: documentation

--- WeatherInfoApp/urls.py
+++ WeatherInfoApp/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .yasg import urlpatterns as yasg_urls
+
 urlpatterns = [
     path('api/weather/', include('apps.weather.urls')),
+    path('api/doc/', include(yasg_urls)),
     path('admin/', admin.site.urls),
 ]
